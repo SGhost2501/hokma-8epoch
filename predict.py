78475db4282b42726f5b664586877308291b198a
SGhost2501 <oneballboi@gmail.com>
2025-06-08 17:35:14 +0100

    Convert to cog format for Replicate

--- predict.py
+++ predict.py
@@ -1,15 +1,18 @@
-import torch
+from cog import BasePredictor, Input
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel
+import torch
 
-model_name = "mistralai/Mistral-7B-v0.1"
-adapter_path = "./"  # Current folder
+class Predictor(BasePredictor):
+    def setup(self):
+        model_name = "mistralai/Mistral-7B-v0.1"
+        adapter_path = "./"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-base_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).cuda()
-model = PeftModel.from_pretrained(base_model, adapter_path).eval()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        base_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).cuda()
+        self.model = PeftModel.from_pretrained(base_model, adapter_path).eval()
 
-def predict(prompt: str) -> str:
-    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
-    output_ids = model.generate(**inputs, max_new_tokens=150)
-    return tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    def predict(self, prompt: Input(str)) -> str:
+        inputs = self.tokenizer(prompt, return_tensors="pt").to("cuda")
+        output_ids = self.model.generate(**inputs, max_new_tokens=150)
+        return self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
